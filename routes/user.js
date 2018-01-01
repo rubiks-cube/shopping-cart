537cc47d24a8708afd30086ec835147aba28e25e
Abhishek Ranjan <abhi03iiitm@gmail.com>
2018-01-01 10:59:47 +0530

    '1'

---
+++ routes/user.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+
+
+var csrf = require('csurf');
+var passport=require('passport');
+var Product=require('../models/products');
+var Cart=require('../models/cart');
+
+var Order=require('../models/order');
+
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
+
+router.get('/signup',notLoggedIn, function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+
+router.post('/signup', passport.authenticate('local.signup', {
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}), function (req, res, next) {
+    if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+    } else {
+        res.redirect('/user/profile');
+    }
+});
+
+router.get('/profile', isLoggedIn, function (req, res, next) {
+    Order.find({user: req.user}, function(err, orders) {
+        if (err) {
+            return res.write('Error!');
+        }
+        var cart;
+        orders.forEach(function(order) {
+            cart = new Cart(order.cart);
+            order.items = cart.generateArray();
+        });
+        res.render('user/profile', { orders: orders });
+    });
+});
+
+router.get('/signin',notLoggedIn, function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/signin', passport.authenticate('local.signin', {
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}), function (req, res, next) {
+    if (req.session.oldUrl) {
+        var oldUrl = req.session.oldUrl;
+        req.session.oldUrl = null;
+        res.redirect(oldUrl);
+    } else {
+        res.redirect('/user/profile');
+    }
+});
+
+router.get('/logout', isLoggedIn, function (req, res, next) {
+    req.logout();
+    res.redirect('/');
+});
+module.exports=router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
+function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
---
+++ seed/product-seeder.js
@@ -0,0 +1,116 @@
+var Product=require('../models/products');
+var mongoose=require('mongoose');
+/*open product-seeder and run node product-seeder.js after removing comments
+mongoose.connect('mongodb://localhost:27017/shop1',(err)=>{
+
+if(err)throw err;
+else{console.log('shop');}
+});
+*/
+var products=[
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg' ,
+  title:'The Alchemist',
+  description:'The Alchemist (Portuguese: O Alquimista) is a novel by Brazilian author Paulo Coelho which was first published in 1988.',
+  price:200
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/54/Manual_of_the_Warrior_of_Light_%28cover%29.jpg' ,
+  title:'Manual of the Warrior Light',
+  description:'Manual of the Warrior of Light is a 1997 collection of Paulo Coelho \'s teachings originally published in various Brazilian newspapers between 1993 and 1996.',
+  price:280
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/89/Aleph_book.jpg/220px-Aleph_book.jpg' ,
+  title:'Aleph',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:300
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Adultery_%28novel%29.jpg/220px-Adultery_%28novel%29.jpg' ,
+  title:'Adultery',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:100
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5b/Paulo_Coelho_-_Brida.jpeg' ,
+  title:'Brida',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:300
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/The_Zahir_%28novel%29.jpg/220px-The_Zahir_%28novel%29.jpg' ,
+  title:'TheZahir',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:200
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Adultery_%28novel%29.jpg/220px-Adultery_%28novel%29.jpg' ,
+  title:'Adultery',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:100
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5b/Paulo_Coelho_-_Brida.jpeg' ,
+  title:'Brida',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:300
+}),
+
+new Product({
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg' ,
+  title:'The Alchemist',
+  description:'The Alchemist (Portuguese: O Alquimista) is a novel by Brazilian author Paulo Coelho which was first published in 1988.',
+  price:200
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/54/Manual_of_the_Warrior_of_Light_%28cover%29.jpg' ,
+  title:'Manual of the Warrior Light',
+  description:'Manual of the Warrior of Light is a 1997 collection of Paulo Coelho \'s teachings originally published in various Brazilian newspapers between 1993 and 1996.',
+  price:280
+}),
+
+new Product({
+
+imagePath: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/89/Aleph_book.jpg/220px-Aleph_book.jpg' ,
+  title:'Aleph',
+  description:'Aleph is a 2011 novel by the Brazilian writer Paulo Coelho. An autobiographical account of Coelho, it is his fourteenth major book, and touches on the theme of spirituality.',
+  price:300
+})
+];
+var done=0;
+
+for(var i=0;i < products.length;i++){
+	var x=products[i];
+	products[i].save(function(err){
+		
+      if(err)throw err;
+      else{done++;
+      if(done==products.length){
+      	exit();
+      }}
+	});
+}
+
+function exit(){
+
+	mongoose.disconnect();
+}
